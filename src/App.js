5f1687aafe0c00aac46c5ba2b0f0d5c8511e0f44
Andrew Baker <abaker@mdsol.com>
2019-12-06 10:47:36 -0500

    add react

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+          <ul>
+            <li>test site 1</li>
+            <li>test site 2</li>
+          </ul>
+      </header>
+    </div>
+  );
+}
+
+export default App;
